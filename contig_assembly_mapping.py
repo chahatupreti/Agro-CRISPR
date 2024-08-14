5b0a08a4fe91abda078eabb571326a20e138b9dd
Chahat Upreti <chahatupretiiitk@gmail.com>
2024-08-14 07:00:16 -0500

    Update contig_assembly_mapping.py

--- contig_assembly_mapping.py
+++ contig_assembly_mapping.py
@@ -6,23 +6,21 @@
 import os
 from collections import defaultdict
 from time import sleep
-from progressbar import *
 
-animalcontiglist=[i[:-4].rstrip() for i in open("/Volumes/bam/DRG/PK/USDA_project/results/2021-11-26/animalcontiglist.txt").readlines()] # this file has the list of all contigs taken from /Volumes/bam/DRG/PK/USDA_project/results/2021-09-01/source_and_Ctype_specific_genomes from C1/C2/C3 animal folders
+animalcontiglist=[i[:-4].rstrip() for i in open("/Volumes/bam/DRG/PK/USDA_project/results/2021-11-26/animalcontiglist.txt").readlines()] 
 animalmapping=defaultdict(list)
 
-humancontiglist=[i[:-4].rstrip() for i in open("/Volumes/bam/DRG/PK/USDA_project/results/2021-11-26/humancontiglist.txt").readlines()] # this file has the list of all contigs taken from /Volumes/bam/DRG/PK/USDA_project/results/2021-09-01/source_and_Ctype_specific_genomes from C1/C2/C3 human folders
+humancontiglist=[i[:-4].rstrip() for i in open("/Volumes/bam/DRG/PK/USDA_project/results/2021-11-26/humancontiglist.txt").readlines()] 
 humanmapping=defaultdict(list)
 
 
 for path, dirs, files in os.walk("/Volumes/bam/DRG/PK/USDA_project/results/2021-09-01/Efs_allanimal_temp"):
     for filename in files:
-#        pbar.update(i) #this adds a little symbol at each iteration
         if not filename.startswith('.'):
             assemblyfile=open(os.path.join(path,filename)).readlines()
             lineswithaccession=''
             for lin in assemblyfile:
-                if lin.startswith('>'): # only saving lines that have accession numbers since other lines wont have contig names
+                if lin.startswith('>'): # only saving lines that have accession numbers since other lines won't have contig names
                     lineswithaccession=lineswithaccession+' '+lin
             for contig in animalcontiglist:
                 if contig in lineswithaccession: # checking if contig
@@ -44,7 +42,7 @@ for path, dirs, files in os.walk("/Volumes/bam/DRG/PK/USDA_project/results/2021-
             assemblyfile=open(os.path.join(path,filename)).readlines()
             lineswithaccession=''
             for lin in assemblyfile:
-                if lin.startswith('>'): # only saving lines that have accession numbers since other lines wont have contig names
+                if lin.startswith('>'): 
                     lineswithaccession=lineswithaccession+' '+lin
             for contig in humancontiglist:
                 if contig in lineswithaccession:
